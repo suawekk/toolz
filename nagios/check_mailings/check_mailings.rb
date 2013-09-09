17bd9e2afc4ebe70357a79bc6a2ba46f9c500d73
suawek <suawek@suawek-notebook.localdomain>
2013-09-09 15:10:38 +0200

    Check mailings: -spf,dkim,spamassassin validation via header regex matching (requires working postfix configuration)

--- nagios/check_mailings/check_mailings.rb
+++ nagios/check_mailings/check_mailings.rb
@@ -46,6 +46,8 @@ if matching.nil?
 	exit 0
 end
 
+matching = matching.merge($config['global'])
+
 
 ts=Time.now.to_i 
 sent=mail.date.to_time.to_i
@@ -53,24 +55,72 @@ threshold = matching['late_delivery_secs'].to_i
 
 
 code = EXIT_OK
-threshold_passed = false
 
+warnings = []
+errors = []
+successes=[]
+
+
+threshold_passed = false
 delay = ts - sent
 if delay  > threshold
-	threshold_passed=true
-	code = EXIT_WARN
+	warnings << "late delivery(#{delay}s)"
 end
 
+if matching['check_spf'] && !mail.header[matching['spf_header']].nil?
+	if  Regexp.new(matching['spf_valid_regex']) !~ mail.header[matching['spf_header']].to_s
+		errors << "SPF fail(#{mail.header[matching['spf_header']]})"
+	else
+		successes << "SPF ok"
+	end
+end
 
-if (threshold_passed)
-	prefix="CHECK_MAILING WARNING(late delivery)"
-else
+if matching['check_dkim'] && !mail.header[matching['dkim_header']].nil?
+	if Regexp.new(matching['dkim_valid_regex']) !~ mail.header[matching['dkim_header']].to_s
+		errors << "DKIM fail(#{mail.header[matching['dkim_header']]})"
+	else
+		successes << "DKIM ok"
+	end
+
+	if mail.header[matching['dkim_signature_header']].nil?
+		errors << "DKIM fail(no signature)"
+	else
+		successes << "DKIM signed"
+	end
+end
+
+if matching['check_spam'] && !mail.header[matching['spam_header']].nil?
+	if  Regexp.new(matching['nospam_regex']) !~  mail.header[matching['spam_header']].to_s
+		errors << "SPAM fail(#{mail.header[matching['spam_header']]})"
+	else
+		successes << "SPAM ok"
+	end
+end
+
+detail_str = ""
+prefix=""
+
+if (!errors.empty?)
+	prefix="CHECK_MAILING ERROR"
+	detail_str += "crit: #{errors.join(',')};"
+end
+
+if (!warnings.empty?)
+	if (prefix.empty?)
+		prefix="CHECK_MAILING WARNING"
+	end
+	detail_str += "warn: #{warnings.join(',')};"
+end
+
+if (warnings.empty? && errors.empty?)
 	prefix ="CHECK_MAILING OK"
 end
 
-details ="#{prefix}: Received mail from #{mail.from.pop}, at: #{mail.date}, subject: #{mail.subject},id: #{mail.message_id}, delayed by #{delay} seconds"
-passive_check_submit_str="[%i] PROCESS_SERVICE_CHECK_RESULT;%s;%s;%i;%s\n" % [ts,$config['submission']['hostname'],matching['svc_name'],code,details]
+detail_str += "ok: #{successes.join(',')}"
 
+
+details ="#{prefix}:#{detail_str} - received mail from #{mail.from.pop}, at: #{mail.date}, subject: #{mail.subject},id: #{mail.message_id}, delayed by #{delay} seconds"
+passive_check_submit_str="[%i] PROCESS_SERVICE_CHECK_RESULT;%s;%s;%i;%s\n" % [ts,$config['submission']['hostname'],matching['svc_name'],code,details]
 
 unless File.exists?($config['submission']['nagios_socket'])
 	STDERR.puts("Nagios socket: #{$config['submission']['nagios_socket']} doesn't exist!") 
@@ -84,9 +134,8 @@ end
 
 unless matching['log_file'].nil?
 	File.open(matching['log_file'],"a+") do |log|
-        log.puts("%s: from %s,subj: %s,id:  %s, delay_s: %i, state: %i\n" % [delay_sTime.now.to_s,mail.from.pop,mail.subject,mail.message_id,delay,code])
+		log.puts("%s: from %s,subj: %s,id:  %s, delay_s: %i, state: %i\n" % [Time.now.to_s,mail.from.pop,mail.subject,mail.message_id,delay,code])
 	end
 end
 
 exit 0
-
